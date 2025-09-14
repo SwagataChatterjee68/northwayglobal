18cb0d6050a21f07dbf6b9ddc987a72d4e809071
SwagataChatterjee68 <ranichatterjee21112@gmail.com>
2025-09-14 14:35:29 +0530

    added some fix

--- src/components/EditorClient.jsx
+++ src/components/EditorClient.jsx
@@ -1,41 +1,4 @@
-// // "use client";
 
-// // import { Editor } from "@tinymce/tinymce-react";
-
-// // export default function EditorClient() {
-// //   return (
-// //     <Editor
-// //       apiKey="qbxnjsfb78uvm2zeaddwkkrwrceywuxsz1o4e9ncq0s3hwmz"
-// //       init={{
-// //         height: 500,
-// //         plugins: "lists link image table code help wordcount",
-// //         toolbar:
-// //           "undo redo | formatselect | bold italic emoticons | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
-// //         skin: "oxide-dark",
-// //         content_css: "dark",
-// //       }}
-// //     />
-// //   );
-// }
-// "use client";
-
-// import { Editor } from "@tinymce/tinymce-react";
-
-// export default function EditorClient() {
-//   return (
-//     <Editor
-//       apiKey="no-api-key"
-//       init={{
-//         height: 500,
-//         plugins: "lists link image table code help wordcount",
-//         toolbar:
-//           "undo redo | formatselect | bold italic emoticons | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
-//         skin: "oxide-dark",
-//         content_css: "dark",
-//       }}
-//     />
-//   );
-// }
 
 import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
